87289c9884a553a14153ea13ed71d62e029a10a3
John Dulaney <john.s.dulaney1@gmail.com>
2017-12-08 13:45:52 -0600

    i lowkey have achieved very little in 5 hours

--- app/app.js
+++ app/app.js
@@ -4,18 +4,7 @@
 
 const app = angular.module("StockApp", ['ngRoute'])
 
-//this is only here because im sick of tabbing to the api source
-//i am an example api url - normal stock
-// const exampleCall = () => {
-//     const stockFunction = "TIME_SERIES_INTRADAY"
-//                         //(1) intraday, (2) daily, (3) weekly, and (4) monthly.
-//     const stockSymbol = "MSFT"
-//                         // any equity symbol you want
-//     const stockInterval = "1min"
-//                         // 5min, 15min, 30min, 60min
-//     const apiKey = "ZZ2RS5PN56S260FBx"
-//                         //fixed
-//                     }
+
 // `https://www.alphavantage.co/query?function=${stockFunction}&symbol=${stockSymbol}&interval=${stockInterval}&apikey=${apiKey}`
 
 //i am an example api url - BITCOIN ONLY - to USD
--- app/stocks/controllers/StocksInvestCtrl.js
+++ app/stocks/controllers/StocksInvestCtrl.js
@@ -15,21 +15,43 @@ angular.module("StockApp")
         StocksFactory.stock()
             .then(
                 response => {
-                    const stockReturn = [];
+                    // empty arrays for our JSON return of a gaggle of objects
+                    const stockReturn = []
+                    const priceReturn = []
+                    // const priceReturn = []
+
+                    // for in loop to get the BTC prices
                     for (let key in StocksFactory.cache[1]) {
                         let element = StocksFactory.cache[1][key];
                         stockReturn.push(element)
                     }
 
-                    // debugger
-                    for (let i = 0; i < stockReturn.length; i++) {
-                        let topStocks = stockReturn[i];
-                        if (stockReturn.length <= 10) {
-                            console.log("loop ran if statement")
-                            return topStocks
-                        }
+                    // Second for in loop on the keys returned, returns the first, which is the price.
+                    for (let key in stockReturn[0]) {
+                        let element = stockReturn[0][key];
+                        priceReturn.push(element)
                     }
-                    console.log(stockReturn);
 
-                })
+                    console.log("current price of btc", stockReturn[0]);
+
+                    // print it to the DOM with fancy pants Angular
+                    $scope.print = `Current Price of BTC = $${priceReturn[0]}`
+
+                    // loop to get price history
+                    for (let i = 0; i < stockReturn.length; i++) {
+                        const btcPreviousHour = stockReturn[i]
+                        //if statement to reduce the 900 whatever results into 20 (an hours worth)
+                        if (i <= 20) {
+                            //for in loop in an attempt to get the first key+value for all 20 runs
+                            for (var key in btcPreviousHour) {
+                                var valueUSD = btcPreviousHour[key];
+                                console.log(key, valueUSD)
+                                $scope.history = `${btcPreviousHour}`
+                                
+                            }
+                        }
+                        // console.log(btcPreviousHour)
+                    }
+                }
+            )
     })
