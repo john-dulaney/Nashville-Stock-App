c72c9caca0bf126e5a78c58acff7f33367275844
John Dulaney <john.s.dulaney1@gmail.com>
2017-12-13 14:59:13 -0600

    responsible commit

--- app/stocks/controllers/StocksDashCtrl.js
+++ app/stocks/controllers/StocksDashCtrl.js
@@ -8,28 +8,21 @@ angular.module("StockApp")
     .controller("StocksDashCtrl", function ($scope, $location, StocksFactory, AuthFactory, $q) {
         watchedStockArray = []
 
-        // we need an automatic on load request that populates dom with saved stocks
         StocksFactory.show()
             .then(response => {
-                
+
                 const allQuoteRequests = []
 
                 for (let key in response.data) {
                     let currentStock = response.data[key]
                     allQuoteRequests.push(StocksFactory.quote(currentStock.stock))
                 }
-
-                $q.all(allQuoteRequests).then(quoteObjectArray => {
-                    $scope.allQuotes = quoteObjectArray
-
-                    $scope.userEmail = AuthFactory.getUser().email
-                })
+                // This is wizard Dark Magic from the 4th realm. Thanks Steve!
+                $q.all(allQuoteRequests)
+                    .then(quoteObjectArray => {
+                        $scope.allQuotes = quoteObjectArray
+                        $scope.userEmail = AuthFactory.getUser().email
+                    })
 
             })
-        })
-        
-        
-
-// we need a button that sends an $http call to server and adds to the db
-
-// we need to be able to click on a saved stock, and be brought to a different partial with moreinfor about the clicked stock
+    })
