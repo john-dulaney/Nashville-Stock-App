ace515ad8b7dfb13638317468727d1fb5e0abd3d
John Dulaney <john.s.dulaney1@gmail.com>
2017-12-20 15:46:31 -0600

    removed app config from gitignore. gotta get this bad boy live

---
+++ app/app.config.js
@@ -0,0 +1,64 @@
+// Author(s): Structure` by Steve Brownlee, High Wizard of NSS. Refactored by John Dulaney
+// Purpose: This module checks the user with firebase. Changes partials based on thei auth
+// ┌(° ͜ʖ͡°)┘
+
+angular.module("StockApp").constant("FIREBASE_CONFIG", {
+    apiKey: "AIzaSyAXrm0yiK2ZN7wqd39EdIVCYOOT4lvxa3Y",
+    authDomain: "everyday-stock-app.firebaseapp.com",
+    databaseURL: "https://everyday-stock-app.firebaseio.com",
+    projectId: "everyday-stock-app",
+    storageBucket: "everyday-stock-app.appspot.com",
+    messagingSenderId: "270476215411"
+})
+
+let isAuth = AuthFactory => new Promise((resolve, reject) => {
+    if (AuthFactory.isAuthenticated()) {
+        // console.log("User is authenticated")
+        resolve()
+    } else {
+        // console.log("User is not authenticated, GET OUT")
+        reject()
+    }
+})
+
+angular.module("StockApp").run(function (FIREBASE_CONFIG) {
+    firebase.initializeApp(FIREBASE_CONFIG)
+})
+
+angular.module("StockApp").config(function ($routeProvider) {
+    $routeProvider.
+    when("/stocks/userdash", {
+            templateUrl: "app/stocks/partials/userdash.html",
+            controller: "StocksQuoteCtrl",
+            resolve: {
+                isAuth
+            }
+        })
+        .when('/stocks/invest', {
+            templateUrl: 'app/stocks/partials/invest.html',
+            controller: 'StocksInvestCtrl',
+            resolve: {
+                isAuth
+            }
+        })
+        .when('/stocks/quote', {
+            templateUrl: 'app/stocks/partials/quote.html',
+            controller: 'StocksQuoteCtrl',
+            resolve: {
+                isAuth
+            }
+        })
+        .when('/register', {
+            templateUrl: 'app/auth/partials/register.html',
+            controller: 'AuthCtrl'
+        })
+        .otherwise('/register')
+})
+
+
+
+// .when('/stocks/detail/:employeeId', {
+//     templateUrl: 'app/stocks/partials/detail.html',
+//     controller: 'EmployeeDetailCtrl',
+//     resolve: { isAuth }
+// })
