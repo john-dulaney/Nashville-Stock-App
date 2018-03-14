76227772d68f19a6b83e64c564b4f3bf3a534237
John Dulaney <john.s.dulaney1@gmail.com>
2018-03-14 10:33:08 -0500

    :rocket: Version 1.0 Release.

--- build/bundle.js
+++ build/bundle.js
@@ -17,6 +17,7 @@
             }, l, l.exports, e, t, n, r)
         }
         return n[o].exports
+        // hello world
     }
     var i = typeof require == "function" && require;
     for (var o = 0; o < r.length; o++) s(r[o]);
